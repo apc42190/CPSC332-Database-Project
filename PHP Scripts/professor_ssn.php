aefbe26323418b2ef0ccdc0ae591936bb254c5da
Aaron <apc42190@yahoo.com>
2021-11-14 18:52:34 -0800

    Fixed goofy commit

--- PHP Scripts/professor_ssn.php
+++ PHP Scripts/professor_ssn.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
     <body>
@@ -38,44 +37,3 @@
         ?>
     </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            $host_name = "mariadb";
-            $user = "cs332t10";
-            $password = "CoolCats332";
-            $database = "cs332t10";
-            
-            $SSN = $_POST['SSN'];
-
-
-            $link = mysqli_connect($host_name, $user, $password, $database);
-
-            if($link->connect_error) {
-                echo "Hello";
-                die("Could not connect: " . connect_error());
-            } else {
-                
-                echo "Connected Successfully\n";
-
-                $query = "SELECT Professor.Title Title, Section.ClassRoom Room, Meeting.MeetDay Day, Section.BegTime Start, Section.EndTime End
-                            FROM Professor P, Section S, Meeting M
-                            WHERE P.SSN = \"$SSN\" AND P.SSN = S.TeacherSSN AND S.CNUM = M.CNUM AND S.SecNum = M.SecNum";
-                
-                $result = $link->query($query);
-
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "Title: " . $row["Title"] . " Room: " . $row["Room"] . " Day: " . $row["Day"] . " Start Time: " . $row["Start"] . " End Time: " . $row["End"];
-                    }
-                } else {
-                    echo "No results";
-                }
-            }
-            $link->close();
-        ?>
-    </body>
-</html>
->>>>>>> 0e4b19a88768e49af9b54eb7270205ef054b4963
