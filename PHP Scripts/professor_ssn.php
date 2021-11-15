0e4b19a88768e49af9b54eb7270205ef054b4963
jareddls <60563919+jareddls@users.noreply.github.com>
2021-11-14 16:39:17 -0800

    updated; fix to php file
    missing semicolon fixed;
    change to call for mysqli to fix connection issue

--- PHP Scripts/professor_ssn.php
+++ PHP Scripts/professor_ssn.php
@@ -2,17 +2,27 @@
 <html>
     <body>
         <?php
-            $link = new mysqli("mariadb", "cs332t10", "CoolCats332", "cs332t10");
-            echo "Hello";
+            $host_name = "mariadb";
+            $user = "cs332t10";
+            $password = "CoolCats332";
+            $database = "cs332t10";
+            
+            $SSN = $_POST['SSN'];
+
+
+            $link = mysqli_connect($host_name, $user, $password, $database);
+
             if($link->connect_error) {
-                die("Could not connect: ".connect_error());
+                echo "Hello";
+                die("Could not connect: " . connect_error());
             } else {
                 
                 echo "Connected Successfully\n";
 
                 $query = "SELECT Professor.Title Title, Section.ClassRoom Room, Meeting.MeetDay Day, Section.BegTime Start, Section.EndTime End
                             FROM Professor P, Section S, Meeting M
-                            WHERE P.SSN = $_POST["SSN"] AND P.SSN = S.TeacherSSN AND S.CNUM = M.CNUM AND S.SecNum = M.SecNum";
+                            WHERE P.SSN = \"$SSN\" AND P.SSN = S.TeacherSSN AND S.CNUM = M.CNUM AND S.SecNum = M.SecNum";
+                
                 $result = $link->query($query);
 
                 if ($result->num_rows > 0) {
@@ -20,11 +30,10 @@
                         echo "Title: " . $row["Title"] . " Room: " . $row["Room"] . " Day: " . $row["Day"] . " Start Time: " . $row["Start"] . " End Time: " . $row["End"];
                     }
                 } else {
-                    echo "No results"
+                    echo "No results";
                 }
-
-                $link->close();
             }
+            $link->close();
         ?>
     </body>
 </html>
